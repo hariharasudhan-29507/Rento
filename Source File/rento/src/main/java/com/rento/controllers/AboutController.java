0ced0cf771b2339ce34dd7538f1370682f00c9cb
Hariharasudhan <sudanayyappan_bcs28@mepcoeng.ac.in>
2026-05-01 21:24:54 +0530

    Update AboutController.java

--- Source File/rento/src/main/java/com/rento/controllers/AboutController.java
+++ Source File/rento/src/main/java/com/rento/controllers/AboutController.java
@@ -12,15 +12,26 @@ public class AboutController {
     @FXML private void onNavHome() { NavigationManager.navigateTo("/fxml/landing.fxml"); }
     @FXML private void onNavBook() {
         if (SessionManager.getInstance().isGuest()) NavigationManager.navigateTo("/fxml/login.fxml");
-        else NavigationManager.navigateTo("/fxml/booking.fxml");
+        else if (SessionManager.getInstance().getCurrentRole() == com.rento.models.User.Role.USER) NavigationManager.navigateTo("/fxml/booking.fxml");
+        else NavigationManager.navigateTo(resolveRoleDashboard());
     }
     @FXML private void onNavRent() {
         if (SessionManager.getInstance().isGuest()) NavigationManager.navigateTo("/fxml/login.fxml");
-        else NavigationManager.navigateTo("/fxml/rent.fxml");
+        else if (SessionManager.getInstance().getCurrentRole() == com.rento.models.User.Role.USER) NavigationManager.navigateTo("/fxml/rent.fxml");
+        else NavigationManager.navigateTo(resolveRoleDashboard());
     }
     @FXML private void onNavContact() { NavigationManager.navigateTo("/fxml/contact.fxml"); }
     @FXML private void onNavProfile() {
         if (SessionManager.getInstance().isGuest()) NavigationManager.navigateTo("/fxml/login.fxml");
         else NavigationManager.navigateTo("/fxml/profile.fxml");
+    }
+
+    private String resolveRoleDashboard() {
+        return switch (SessionManager.getInstance().getCurrentRole()) {
+            case ADMIN -> "/fxml/admin_dashboard.fxml";
+            case DRIVER -> "/fxml/driver_dashboard.fxml";
+            case SUPPLIER -> "/fxml/supplier_dashboard.fxml";
+            default -> "/fxml/profile.fxml";
+        };
     }
 }
